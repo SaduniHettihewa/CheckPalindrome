d2cac79cb1f9f19b76b3642e70b551e40c248f7f
SaduniHettihewa <hhsnimasha98@gmail.com>
2021-06-16 19:02:22 +0530

    add

---
+++ Palindrome.java
@@ -0,0 +1,12 @@
+package palindrome;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        int r, sum = 0, temp;
+        int n = 565;// Check this no is palindrome or not  
+
+        temp = n;
+    }
+
+}
